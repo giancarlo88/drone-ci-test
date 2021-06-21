68b90e3e5007f3793e3e5131231cd75546abf2c5
Giancarlo Galliani <galliani.08@gmail.com>
2021-06-21 18:09:22 +0100

    Update config

--- cypress/integration/test.spec.js
+++ cypress/integration/test.spec.js
@@ -2,11 +2,11 @@ describe('Test environment', () => {
   before(() => cy.visit('http://localhost:3000/test'))
 
   it('renders a button', () => {
-    cy.get('button').should('exist')
+    cy.get('button').contains('Show message').should('exist')
   })
 
   it('clicks the button', () => {
-    cy.get('button').click()
+    cy.get('button').contains('Show message').click()
   })
 
   it('makes text appear', () => {
--- pages/test.vue
+++ pages/test.vue
@@ -1,7 +1,31 @@
 <template>
   <div class="test">
     <button @click="showMessage = !showMessage">Show message</button>
+    <button>Another button</button>
+    <button>A third button</button>
     <p v-if="showMessage">Test message</p>
+    <div class="images">
+      <img
+        src="https://scatterjar.com/wp-content/uploads/2016/05/www.scatterjar.com-0523-585x878.jpg"
+      />
+      <img
+        src="https://scatterjar.com/wp-content/uploads/2016/09/www.scatterjar.com-3845-585x390.jpg"
+      />
+      <img
+        src="https://images.freeimages.com/images/large-previews/dfa/jungle-1377573.jpg"
+      />
+    </div>
+    <div class="images">
+      <img
+        src="https://images.freeimages.com/images/large-previews/39a/no-grain-no-pain-1326753.jpg"
+      />
+      <img
+        src="https://images.freeimages.com/images/large-previews/9c3/sunshine-1408040.jpg"
+      />
+      <img
+        src="https://images.freeimages.com/images/large-previews/5b8/starfish-1377326.jpg"
+      />
+    </div>
   </div>
 </template>
 
@@ -15,4 +39,10 @@ export default {
 }
 </script>
 
-<style></style>
+<style>
+.images {
+  display: flex;
+  flex-direction: row;
+  height: 500px;
+}
+</style>
